9ee33daffa6e72782fa9d8bdbc01ccad27134f3c
Asim Mahmood <mahmood.asim88@gmail.com>
2023-03-17 15:02:55 +0500

    Home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Home from "./pages/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="max-wscreen-md max-auto pt-20">
-      <Home />
-    </div>
+    <Router>
+      <div className="max-wscreen-md max-auto pt-20">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
