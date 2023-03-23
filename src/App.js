92fc48b06cce49a1b0dffb9ea26b74711d9639b9
Asim Mahmood <mahmood.asim88@gmail.com>
2023-03-23 14:04:54 +0500

    About page added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from "./pages/Home";
 import Article from "./pages/Article";
 import ArticlesList from "./pages/ArticlesList";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import About from "./pages/About";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className="max-wscreen-md max-auto pt-20">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
           <Route path="/article" element={<Article />} />
           <Route path="/article-list" element={<ArticlesList />} />
         </Routes>
