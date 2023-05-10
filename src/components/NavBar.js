6c9eb7ce6c41c5dce06a43e1640977e593da2a6d
Asim Mahmood <mahmood.asim88@gmail.com>
2023-05-10 12:57:52 +0500

    Navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,5 @@
 import React from "react";
-// import Link from "react-router-dom";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
     <nav className="border-b-4 border-green-700 text-center fixed top-0 bg-green-600 font-bold w-full text-lg text-white">
@@ -16,7 +15,7 @@ const NavBar = () => {
           </Link>
         </li>
         <li className="inline-block py-4 ">
-          <Link to="/articles-list" className="pl-6 pr-8">
+          <Link to="/article-list" className="pl-6 pr-8">
             Articles
           </Link>
         </li>
