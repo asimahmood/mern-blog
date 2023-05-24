02e13d5ba42a948d7626891b086faf937d5e43e5
unknown <asim.mahmood@farmtohome.com.pk>
2023-05-24 22:45:55 +0500

    articles

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import articleContent from "./article-content";
+
+// Pages
+
+// Components
+import Articles from "../components/Articles";
 
 const Article = () => {
+  const { name } = useParams();
+  const article = articleContent.find((article) => article.name === name);
+
+  const otherArticles = articleContent.filter(
+    (article) => article.name !== name
+  );
   return (
-    <div>
+    <>
       <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-900">
-        This is Article
+        {article.title}
       </h1>
-    </div>
+      {article.content.map((paragraph, index) => (
+        <p className="mx-auto leading-relaxed text-base mb-4" key={index}>
+          {paragraph}
+        </p>
+      ))}
+      <h1 className="sm:text-2xl text-xl font-bold my-4 text-gray-900">
+        Other Articles
+      </h1>
+      <div className="flex flex-wrap -m-4">
+        <Articles articles={otherArticles} />
+      </div>
+    </>
   );
 };
 
---
+++ src/pages/article-content.js
@@ -0,0 +1,28 @@
+const articles = [
+  {
+    name: "learn-react",
+    title: "The Fastest Way to Learn React",
+    content: [`Welcome! Today we are going to learn react`],
+    thumbnail: "/images/blog-1.png",
+  },
+  {
+    name: "learn-node",
+    title: "The Fastest Way to Learn Node",
+    content: [`Welcome! Today we are going to learn Node`],
+    thumbnail: "/images/blog-1.png",
+  },
+  {
+    name: "learn-Laravel",
+    title: "The Fastest Way to Learn Laravel",
+    content: [`Welcome! Today we are going to learn Laravel`],
+    thumbnail: "/images/blog-3.jpg",
+  },
+  {
+    name: "learn-Js",
+    title: "The Fastest Way to Learn Js",
+    content: [`Welcome! Today we are going to learn Js`],
+    thumbnail: "/images/blog-3.jpg",
+  },
+];
+
+export default articles;
